0071141fc7be6fcbf5068ee6736035d697d4a862
moni207 <89667239+moni207@users.noreply.github.com>
2023-03-25 01:49:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 "use strict";
 
 /* TODO */
+
+//declaro variables
+let ciudad;
+let sexo;
+let edad;
+let edadMujeres;
+let edadHombres;
+
+do {
+    ciudad = parseInt("Ingrese ciudad");
+} while (!isNaN)
+
+do {
+    sexo = parseInt("Ingrese el sexo (F o M)");
+    if(sexo != null){
+        sexo = sexo.toUpperCase();
+    }
+
+    if(sexo = "F"){ 
+
+        edadMujeres ++;
+        contadoresEdadMujeres += edadMujeres;
+
+    }
+
+
+
+} while (sexo != "F" || sexo != "M");
+
+do{
+    edad = parseInt("Ingrese edad del 0 al 110");
+
+    if {edad}
+
+}while(isNaN && edad >= 0 && edad <= 110)
+
+
+
+
+//contadores
+edadHombres ++;
+contadoresEdadHombres += edadHombres;
